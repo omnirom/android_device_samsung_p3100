8073866ff8e62f209714e8f178c8fa3702737d86
Markus Guidry <markus@gu1dry.com>
2014-03-08 23:42:08 -0500

    p3100: Align ro.product with stock
    Additionally this updates the fingerprint to P3100XWDMK1 (4.2.2)
    
      PS2: Add README
      PS3: ro.product.device is actually set with TARGET_DEVICE & not
           PRODUCT_DEVICE; this has been corrected.
      PS4: PRODUCT_RELEASE_NAME isn't used in omnirom.
    
    Change-Id: Iaad0fbf7cc37c1321c99bc47579d2d7035db2609

--- omni_p3100.mk
+++ omni_p3100.mk
@@ -18,21 +18,22 @@
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := p3100
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/p3100/full_p3100.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := p3100
-PRODUCT_NAME := omni_p3100
-PRODUCT_BRAND := samsung
+# Device identifier. This must come after all inclusions
 PRODUCT_MODEL := GT-P3100
+PRODUCT_BRAND := samsung
+PRODUCT_NAME := omni_p3100
+PRODUCT_DEVICE := p3100
 PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=espressorfxx TARGET_DEVICE=espressorf BUILD_FINGERPRINT=samsung/espressorfxx/espressorf:4.0.3/IML74K/P3100XWALE2:user/release-keys PRIVATE_BUILD_DESC="espressorfxx-user 4.0.3 IML74K P3100XWALE2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=espressorfxx \
+    TARGET_DEVICE=espressorf \
+    PRIVATE_BUILD_DESC="espressorfxx-user 4.2.2 JDQ39 P3100XWDMK1 release-keys" \
+    BUILD_FINGERPRINT="samsung/espressorfxx/espressorf:4.2.2/JDQ39/P3100XWDMK1:user/release-keys"
